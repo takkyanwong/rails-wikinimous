ab65deaff1fdc1b8488f7bb6669c6b8bc1ee5797
Tay Yang Wong <wongtakkyan@gmail.com>
2020-10-20 13:43:21 +0200

    Wikinomius completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'articles', to: 'articles#index'
+  # get 'articles', to: 'articles#new', as: :new_article
+  # get 'articles/new', to: 'articles#show', as: :article
+  # post 'articles', to: 'articles#create'
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
+
+  root to: 'articles#index', as: :articles
+
+  resources :articles, except: [:index]
 end
